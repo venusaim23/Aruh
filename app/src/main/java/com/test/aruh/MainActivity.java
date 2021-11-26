8e721b344d29c25f80fb78cf45916bae69363290
Venu Sai <venusaim23@gmail.com>
2021-11-26 13:40:05 +0530

    modified MainActivity.java and misc.xml

--- app/src/main/java/com/test/aruh/MainActivity.java
+++ app/src/main/java/com/test/aruh/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = binding.nav_view;
+		//findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
